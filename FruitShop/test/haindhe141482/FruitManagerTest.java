517c8fb2a86ae889782f5312a4831994c2ae703b
haindhe141482 <90599135+haindhe141482@users.noreply.github.com>
2021-10-17 22:21:11 +0700

    test

---
+++ FruitShop/test/haindhe141482/FruitManagerTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package haindhe141482;
+
+import controller.FruitManager;
+import model.Fruit;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author win
+ */
+public class FruitManagerTest {
+
+    FruitManager fm;
+
+    public FruitManagerTest() {
+        fm = new FruitManager();
+    }
+
+    @Test
+    public void testAddFruit() {
+    }
+
+    @Test
+    public void testUpdateFruit() {
+    }
+
+    @Test
+    public void testDeleteFruit() {
+    }
+
+    @Test
+    public void testGetFruitList() {
+    }
+
+    @Test
+    public void testGetFruitByItem() {
+        Fruit f = fm.getFruitByItem(0);
+        Fruit expected = null;
+        assertEquals(expected, f);
+
+    }
+
+}
